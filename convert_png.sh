16fd290cdaae7c35fbb897db7dcf7c9f6019985c
DirtBagXon <noop@member.fsf.org>
2021-04-11 13:13:59 +0100

    Major tidy on pussinboots, dragontrainer & samuraijack

--- convert_png.sh
+++ convert_png.sh
@@ -3,9 +3,8 @@
 scale=3
 
 for x in $(ls *.png); do
-
 	s=$(file $x | awk '{print $5}')
 	b=$(echo "$s/$scale" | bc)
-	$(convert $x  -resize $b ../$x)
+	$(convert $x -resize $b ../$x)
 	$(pngcrush -ow -rem allb -reduce ../$x)
 done
